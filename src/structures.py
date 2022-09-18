cd3563b0376541e332430f70df709e9166faf50e
David Isenberg <d.avidisenberg@gmail.com>
2022-09-18 18:03:05 -0400

    initial checkin

---
+++ src/structures.py
@@ -0,0 +1,15 @@
+
+def valid_parens(input: str):
+    from collections import deque
+    keys = { '}':'{', ']':'[','(':')'}
+    stack = deque()
+
+    for s in input:
+        if s in keys:
+            if len(stack) == 0:
+                return False
+            if stack[-1] == keys[s]:
+                stack.pop()
+        else:
+            stack.append(s)
+    return len(stack) == 0
