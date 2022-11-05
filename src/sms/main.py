62142e8447abc59f64419e64860172944b8a42ea
David Isenberg <d.avidisenberg@gmail.com>
2022-11-05 13:40:05 -0400

    flask app created

---
+++ src/sms/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+import sender
+
+app = Flask(__name__)
+
+@app.route('/send/')
+def send():
+    username = request.args.get('user')
+    message = request.args.get('message')
+
+    try:
+        if username == "nothingbutnet":
+            sender.send_message(message,"defined")
+        else:
+            return "unauthorized"
+        return "sent message"
+    except Exception as e:
+        return "failed to send: " + str(e)
+
+
+
+if __name__ == '__main__':
+    app.run()
--- src/sms/sender.py
+++ src/sms/sender.py
@@ -1,8 +1,7 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
 
-
+# Download the helper library from https://www.twilio.com/docs/python/install
 # ? Shorthand identifier for your profile: My first Twilio account
 # Created API Key SK771f8a8e2cc65eee00220c112495c55e and stored the secret in Config. See: https://www.twilio.com/console/runtime/api-keys/SK771f8a8e2cc65eee00220c112495c55e
 # twilio-cli configuration saved to "C:\Users\david\.twilio-cli\config.json"
@@ -11,20 +10,22 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
+# https://www.twilio.com/blog/register-phone-number-send-sms-twilio-cli
+# twilio profiles:use "My first Twilio account"
+# twilio api:core:incoming-phone-numbers:create --phone-number="+14846015086"
+
 account_sid = 'ACb42f9115cf27b3c34ddfc388e381aeee' #os.environ['ACb42f9115cf27b3c34ddfc388e381aeee']
 auth_token = 'da3421679cdd3837e09526998e6f26cf' #os.environ['da3421679cdd3837e09526998e6f26cf']
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+14846015086',
-                     to='+17189862989'
-                 )
+def send_message(input_body, input_to):
+    message = client.messages \
+                    .create(
+                        body=input_body,
+                        from_='+14846015086',
+                        to='+17189862989'
+                    )
 
-print(message.sid)
-# https://www.twilio.com/blog/register-phone-number-send-sms-twilio-cli
-# twilio profiles:use "My first Twilio account"
+    print(message.sid)
 
-#twilio api:core:incoming-phone-numbers:create --phone-number="+14846015086"
 
