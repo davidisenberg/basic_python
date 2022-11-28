1611097e9c89f1449c83135aa2c8d7e7fb4e3611
David Isenberg <d.avidisenberg@gmail.com>
2022-11-28 18:12:55 -0500

    add mom

--- src/sms/recipients.py
+++ src/sms/recipients.py
@@ -5,7 +5,7 @@ def get_recipients(user):
     user = str(user)
     if user == "nothingbutnet":
         #return ["+17189862989","+17189862991","+19174154012"]
-        return ["+17189862989"]
+        return ["+17189862989","+17189862991"]
     if user == "superstar":
         return ["+17189862989"]
     if user == "(718) 986-2989":
